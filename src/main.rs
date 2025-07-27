866d00a8f7a242d473a337ab21129ba55ebd2650
Alejandro Rojas <alex.olmedo.rojas@gmail.com>
2025-07-26 18:31:45 -0600

    Added router and listener to main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+
+    let router = Router::new()
+        .route("/", get(root));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:9999").await?;
+    axum::serve(listener, router).await?;
+
+    Ok(())
 }
+    
+async fn root() -> &'static str {
+    "TODO API!!!"
+}
+
+    
+
